f29510edbb2b18a2777ce973b09ea654a9dc92ed
Ldsah <buzinga3496@gmail.com>
2022-06-30 19:42:28 +0300

    Initial commit

---
+++ src/main/java/Platform.java
@@ -0,0 +1,3 @@
+public interface Platform {
+    void printName();
+}
---
+++ src/main/java/com/example/springdi3/SpringDi3Application.java
@@ -0,0 +1,13 @@
+package com.example.springdi3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDi3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDi3Application.class, args);
+    }
+
+}
